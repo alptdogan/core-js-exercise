5261ef8b0a4157403112e7cd64b9d829047c11ef
alptdogan <alpdogan000@gmail.com>
2023-07-06 13:40:02 +0300

    Create SumOfFirstFiftyNums.js

---
+++ basicPractice/SumOfFirstFiftyNums.js
@@ -0,0 +1,9 @@
+// 1 den 50 ye kadar olan sayıları toplamını hesaplayan programi for döngüsünü kullanarak yapınız?
+
+let sum = 0;
+
+for (let i = 0; i < 50; i++) {
+    sum += i;
+}
+
+console.log(sum);
