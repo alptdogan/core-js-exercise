7f0178188142909cd3e5dea54428c1b9b2c93439
alptdogan <alpdogan000@gmail.com>
2023-07-06 13:22:37 +0300

    Init

---
+++ basicPractice/SumOfTwoNums.js
@@ -0,0 +1,7 @@
+// Klavyeden girilen 2 sayının toplamını hesaplayan ve sonucunu ekrana yazdıran javascript programını yazınız?
+
+var s1 = parseInt(prompt("Sayi 1. giriniz:"));
+var s2 = parseInt(prompt("Sayi 2. giriniz:"));
+var toplam = s1 + s2;
+
+console.log(toplam);
